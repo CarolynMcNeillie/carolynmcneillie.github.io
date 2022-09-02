8f6253f448eb6cd92161c4bd83266c5e223629ed
Carolyn McNeillie <carolynmcneillie@Carolyns-MacBook-Pro.local>
2022-09-02 13:12:59 -0400

    setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,13 @@ module.exports = {
     title: `Ketchum`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: `gatsby-source-pokeapi`,
+      options: {
+        nbOfPokemons: 251,
+      },
+    },
+  ]
 };
