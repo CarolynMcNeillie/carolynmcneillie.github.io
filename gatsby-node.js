0f5dd8a492c759ec5c4dc1be9232c064ad5e1661
Carolyn McNeillie <carolynmcneillie@Carolyns-MacBook-Pro.local>
2022-09-02 20:51:44 -0400

    wip

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,30 @@
 const fetch = require('node-fetch');
 
-const NODE_TYPE = 'Pokemon';
+const POKEMON_TYPE = 'Pokemon';
+const MOVE_NODE = 'Moves';
 
 exports.sourceNodes = async ({ actions, createContentDigest, createNodeId }) => {
   const { createNode } = actions;
 
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?' + new URLSearchParams({
+  const pokemonResponse = await fetch('https://pokeapi.co/api/v2/pokemon?' + new URLSearchParams({
     limit: '2000',
 }));
-  const json = await response.json();
-  const { results = [] } = json;
+  const pokemonJson = await pokemonResponse.json();
+  const pokemonResults = pokemonJson.results || []
 
-  const pokemon = await Promise.all(results.map(async result => {
+  const pokemon = await Promise.all(pokemonResults.map(async result => {
+    const { url } = result;
+    const pokeResponse = await fetch(url);
+    return await pokeResponse.json();
+  }));
+
+  const response2 = await fetch('https://pokeapi.co/api/v2/move?' + new URLSearchParams({
+    limit: '1000',
+    }));
+  const json2 = await response2.json();
+  const results2 = json2.results || []
+
+  const moves = await Promise.all(results2.map(async result => {
     const { url } = result;
     const pokeResponse = await fetch(url);
     return await pokeResponse.json();
@@ -20,11 +33,25 @@ exports.sourceNodes = async ({ actions, createContentDigest, createNodeId }) => 
   pokemon.forEach((node, index) => {
     createNode({
       ...node,
-      id: createNodeId(`${NODE_TYPE}-${node.id}`),
+      id: createNodeId(`${POKEMON_TYPE}-${node.id}`),
       parent: null,
       children: [],
       internal: {
-        type: NODE_TYPE,
+        type: POKEMON_TYPE,
+        content: JSON.stringify(node),
+        contentDigest: createContentDigest(node)
+      }
+    });
+  });
+
+  moves.forEach((node, index) => {
+    createNode({
+      ...node,
+      id: createNodeId(`${MOVE_NODE}-${node.id}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: MOVE_NODE,
         content: JSON.stringify(node),
         contentDigest: createContentDigest(node)
       }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,6 @@
 import React, {useState} from "react";
 import { useStaticQuery, graphql } from 'gatsby';
 
-function battle(e) {
-  const player1 = document.querySelector("#player1");
-  const player2 = document.querySelector("#player2");
-  e.preventDefault();
-  alert(`TIME TO BATTLE, ${player1.value} and ${player2.value}!!!`);
-}
-
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -67,6 +60,29 @@ const IndexPage = () => {
       <p><strong>Moves </strong>{ pokemon.moves && pokemon.moves.join(', ') }</p>
     </article>
   )
+
+  function getPokemon(name) {
+    return allPokemon.find(pokemon => pokemon.name === name)
+  }
+  
+  function getRandomMove(moves) {
+    return moves[Math.floor(Math.random()*moves.length)];
+  }
+  
+  function battle(e) {
+    e.preventDefault();
+    const player1 = document.querySelector("#player1");
+    const player2 = document.querySelector("#player2");
+    const pokemon1 = getPokemon(player1.value)
+    const player1Move = getRandomMove(pokemon1.moves)
+    const pokemon2 = getPokemon(player2.value)
+    const player2Move = getRandomMove(pokemon2.moves)
+    console.log(pokemon1)
+    alert(`TIME TO BATTLE, ${player1.value} and ${player2.value}!!!`);
+    alert(`First up, ${player1.value} uses ${player1Move}`);
+    alert(`First up, ${player2.value} uses ${player2Move}`);
+  }
+
 
   return (
     <main>
