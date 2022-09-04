6cdb5081edd9c2f89425913dda36140ca0130ffa
Carolyn McNeillie <carolynmcneillie@Carolyns-MacBook-Pro-2.local>
2022-09-04 19:07:41 -0400

    wip

---
+++ src/styles/novel.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Page = styled.article`
+  max-width: 800px;
+  margin: 0 auto;
+`;
+
+export const Golden = styled.span`
+  background-color: gold;
+`;
